d0e899a49d81b52fa4db1bf1e5f504abc5651795
gustavo-cortesprog <al45335@un.edu.mx>
2026-09-15 20:49:40 -0600

    Agregar archivos de proyecto.

---
+++ CalculadoraIMC_POO2_Gustavo Andrade/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalculadoraIMC_POO2_Gustavo_Andrade.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ CalculadoraIMC_POO2_Gustavo Andrade/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalculadoraIMC_POO2_Gustavo_Andrade.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
